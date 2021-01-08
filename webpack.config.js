9f0463bdd4bd12ab1c62423763c5a508a283ec46
Sochiru <unleash9224@gmail.com>
2021-01-09 01:30:43 +0900

    Initialize

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = (_, argv) => {
+  let devMode = argv.mode === 'development'
+
+  const options = {
+    entry: path.resolve('src', 'index.tsx'),
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+      filename: '[name].[hash].js',
+      chunkFilename: '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, './tsconfig.json')
+            }
+          }
+        },
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(ico|png|jpg|jpeg|json)?$/,
+          loader: 'file-loader',
+          options: {
+            name: '[hash].[ext]'
+          }
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: '[name].[hash].css'
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'public'
+          }
+        ]
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    }
+  }
+
+  options.mode = argv.mode
+
+  if (devMode) {
+    options.devtool = 'source-map'
+    options.devServer = {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    }
+  }
+
+  return options
+}
