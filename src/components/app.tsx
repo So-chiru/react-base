cde575c57f69fdd7e6b5cbefd699337039ce317f
So-chiru <unleash9224@gmail.com>
2021-04-23 10:05:15 +0900

    Add redux store, saga

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-
-const App: React.FC = () => {
-  return <div className='App'></div>
+// 애플리케이션의 최상위 (따지면 아니지만) 가 될 컴포넌트입니다. 이 안에서 원하는 컴포넌트들을
+// 정의하여 사용하시면 됩니다.
+const App = () => {
+  return <div className='app'></div>
 }
 
 export default App
---
+++ src/store/sagas.ts
@@ -0,0 +1,21 @@
+import createSagaMiddleware from 'redux-saga'
+import { all } from 'redux-saga/effects'
+// import { call, put, takeEvery } from 'redux-saga/effects'
+
+const sagaMiddleware = createSagaMiddleware()
+
+// 첫 번째 인자를 가진 Action이 Dispatch 되어 요청이 들어오면 그 Action을 처리할 수 있도록 호출할 함수를 2번째 인자로 지정합니다.
+// 두 번째 인자가 가지는 함수는 generator이며 이 주석 위에다 정의하면 편합니다. 인자는 action 객체를 가집니다.
+export function * defaultSaga () {
+  // yield takeEvery('@llct/api_lists/request', fetchAPIData)
+}
+
+// 하위 saga들을 모두 실행합니다.
+export function * rootSaga () {
+  yield all([defaultSaga()])
+}
+
+// 최상위 saga를 실행합니다.
+sagaMiddleware.run(rootSaga)
+
+export default sagaMiddleware
